0a4b99c6fd2864cc753822c709f64f2457ebed4a
andreleao81 <andrelao@poli.ufrj.br>
2022-06-29 23:45:57 -0300

    changes

--- app/suporte/model.py
+++ app/suporte/model.py
@@ -4,4 +4,9 @@ from app.model import BaseModel
 class Suporte(BaseModel):
     __tablename__ = 'suporte'
     
+    codigo_suporte = db.Column()
+    telefone = db.Column(db.Integer())
+    senha = db.Column()
+
+
     # sei la
